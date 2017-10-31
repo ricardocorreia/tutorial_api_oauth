5288d6295a91e5f091cbb47d2947e42487757f99
Ricardo Correia <ricardo.correia@cleverti.com>
2017-10-31 09:51:39 +0000

    Applied code standards

--- src/cleverti/OAuthBundle/Command/ClientCreateCommand.php
+++ src/cleverti/OAuthBundle/Command/ClientCreateCommand.php
@@ -1,13 +1,15 @@
 <?php
 namespace cleverti\OAuthBundle\Command;
+
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+
 class ClientCreateCommand extends ContainerAwareCommand
 {
-    protected function configure ()
+    protected function configure()
     {
         $this
             ->setName('oauth:client:create')
@@ -16,7 +18,7 @@ class ClientCreateCommand extends ContainerAwareCommand
             ->addOption('grant-type', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Set allowed grant type. Use multiple times to set multiple grant types', null)
         ;
     }
-    protected function execute (InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
         $clientManager = $this->getContainer()->get('fos_oauth_server.client_manager.default');
         $client = $clientManager->createClient();
@@ -26,4 +28,3 @@ class ClientCreateCommand extends ContainerAwareCommand
         $output->writeln("Added a new client with  public id <info>".$client->getPublicId()."</info> and secret <info>".$client->getSecret()."</info>");
     }
 }
-
